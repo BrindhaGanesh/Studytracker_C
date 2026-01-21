c4be370876522c7eca546ff772794bb9370a5e42
BrindhGanesh <mailtobrindhaganesh@gmail.com>
2026-01-21 21:46:15 +0100

    Initial Blazor Study Tracker

---
+++ StudyTask.cs
@@ -0,0 +1,10 @@
+public class StudyTask {
+    public string Name { get; set; }
+    public double TargetHours { get; set; }
+    public double SecondsSpent { get; set; }
+    public DateTime? LastStarted { get; set; }
+    
+    // Logic for the UI
+    public double Progress => (SecondsSpent / 3600) / TargetHours;
+    public string TimeRemaining => $"{Math.Max(0, TargetHours - (SecondsSpent / 3600)):F1} hrs left";
+}
